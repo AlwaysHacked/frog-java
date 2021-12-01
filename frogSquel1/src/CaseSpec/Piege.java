8b2beb4f40ac39b4cbd1772152fb3091d9ae47fd
Ala-eddine Lamri <ala-eddine.lamri@universite-paris-saclay.fr>
2021-12-02 00:43:08 +0100

    "pont en rose et piège en rouge, fonctionnel"

--- frogSquel1/src/CaseSpec/Piege.java
+++ frogSquel1/src/CaseSpec/Piege.java
@@ -1,10 +1,20 @@
 package CaseSpec;
 
 import util.Case;
+import graphicalElements.Element;
+import gameCommons.Game;
+
+
+import java.awt.*;
 
 public class Piege extends CaseSpec {
-
-    public Piege(Case c) {
-        super(c);
+    private final Color colorP=Color.RED;
+    public Piege(Case c, Game g) {
+        super(c, g);
     }
+
+    public void addGraphics() {
+            Color color = colorP;
+            super.game.getGraphic().add(new Element(super.pos.absc, super.pos.ord, color));
+        }
 }
--- frogSquel1/src/CaseSpec/Tunnel.java
+++ frogSquel1/src/CaseSpec/Tunnel.java
@@ -1,10 +1,19 @@
 package CaseSpec;
 
 import util.Case;
+import graphicalElements.Element;
+import gameCommons.Game;
+
+import java.awt.*;
 
 public class Tunnel extends CaseSpec{
+    private final Color colorP=Color.MAGENTA;
+    public Tunnel(Case c, Game g) {
+        super(c, g);
+    }
 
-    public Tunnel(Case c) {
-        super(c);
+    public void addGraphics() {
+        Color color = colorP;
+        super.game.getGraphic().add(new Element(super.pos.absc, super.pos.ord, color));
     }
 }
